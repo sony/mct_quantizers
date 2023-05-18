af1333b7e717c6deee6f205c99afa13adc388577
Ofir Gordon <Ofir.Gordon@altair-semi.com>
2023-05-18 16:42:25 +0300

    Implementing test suite

---
+++ tests/test_suite.py
@@ -0,0 +1,25 @@
+import importlib
+import unittest
+
+found_tf = importlib.util.find_spec("tensorflow") is not None and importlib.util.find_spec(
+    "tensorflow_model_optimization") is not None
+found_pytorch = importlib.util.find_spec("torch") is not None and importlib.util.find_spec(
+    "torchvision") is not None
+
+if __name__ == '__main__':
+    # -----------------  Load all the test cases
+    suiteList = []
+    test_loader = unittest.TestLoader()
+    MCTQuantizersSuite = unittest.TestSuite(suiteList)
+
+    # Add TF tests only if tensorflow is installed
+    if found_tf:
+        package_tests = test_loader.discover("tests.keras", pattern="test_*.py")
+        MCTQuantizersSuite.addTests(package_tests)
+
+    # Add Pytorch tests only if Pytorch is installed
+    if found_pytorch:
+        package_tests = test_loader.discover("tests.pytorch", pattern="test_*.py")
+        MCTQuantizersSuite.addTests(package_tests)
+
+    unittest.TextTestRunner(verbosity=0).run(MCTQuantizersSuite)
