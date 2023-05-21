280f83e00ef85088e4015519d19c8f00f9c24bab
Ofir Gordon <Ofir.Gordon@altair-semi.com>
2023-05-21 16:23:35 +0300

    Attempt to fix test suite in action

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,10 +1,17 @@
 import importlib
 import unittest
 
+
+
 found_tf = importlib.util.find_spec("tensorflow") is not None and importlib.util.find_spec(
     "tensorflow_model_optimization") is not None
 found_pytorch = importlib.util.find_spec("torch") is not None and importlib.util.find_spec(
     "torchvision") is not None
+
+if found_tf:
+    from tests.keras_tests.test_get_quantizers import TestKerasGetInferableQuantizer
+if found_pytorch:
+    from tests.pytorch_tests.test_get_quantizers import TestPytorchGetInferableQuantizer
 
 if __name__ == '__main__':
     # -----------------  Load all the test cases
@@ -14,12 +21,14 @@ if __name__ == '__main__':
 
     # Add TF tests only if tensorflow is installed
     if found_tf:
-        package_tests = test_loader.discover("tests.keras_tests", pattern="test_*.py")
-        MCTQuantizersSuite.addTests(package_tests)
+        # package_tests = test_loader.discover("tests.keras_tests", pattern="test_*.py")
+        # MCTQuantizersSuite.addTests(package_tests)
+        MCTQuantizersSuite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestKerasGetInferableQuantizer))
 
     # Add Pytorch tests only if Pytorch is installed
     if found_pytorch:
-        package_tests = test_loader.discover("tests.pytorch_tests", pattern="test_*.py")
-        MCTQuantizersSuite.addTests(package_tests)
+        # package_tests = test_loader.discover("tests.pytorch_tests", pattern="test_*.py")
+        # MCTQuantizersSuite.addTests(package_tests)
+        MCTQuantizersSuite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestPytorchGetInferableQuantizer))
 
     unittest.TextTestRunner(verbosity=0).run(MCTQuantizersSuite)
