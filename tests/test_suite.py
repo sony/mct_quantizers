68f1c89b8b8c29fdd559a7a9bc867fa593dd37c5
Ofir Gordon <Ofir.Gordon@altair-semi.com>
2023-05-21 16:40:24 +0300

    Another try for test suite

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,12 +1,9 @@
 import importlib
 import unittest
 
-
-
 found_tf = importlib.util.find_spec("tensorflow") is not None and importlib.util.find_spec(
     "tensorflow_model_optimization") is not None
-found_pytorch = importlib.util.find_spec("torch") is not None and importlib.util.find_spec(
-    "torchvision") is not None
+found_pytorch = importlib.util.find_spec("torch")
 
 if found_tf:
     from tests.keras_tests.test_get_quantizers import TestKerasGetInferableQuantizer
@@ -17,18 +14,18 @@ if __name__ == '__main__':
     # -----------------  Load all the test cases
     suiteList = []
     test_loader = unittest.TestLoader()
-    MCTQuantizersSuite = unittest.TestSuite(suiteList)
+    MCTQuantizersSuite = unittest.TestSuite()
 
     # Add TF tests only if tensorflow is installed
     if found_tf:
-        # package_tests = test_loader.discover("tests.keras_tests", pattern="test_*.py")
-        # MCTQuantizersSuite.addTests(package_tests)
-        MCTQuantizersSuite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestKerasGetInferableQuantizer))
+        # suiteList.append(unittest.TestLoader().loadTestsFromTestCase(TestKerasGetInferableQuantizer))
+        package_tests = test_loader.discover("tests.keras_tests", pattern="test_*.py")
+        MCTQuantizersSuite.addTests(package_tests)
 
     # Add Pytorch tests only if Pytorch is installed
     if found_pytorch:
-        # package_tests = test_loader.discover("tests.pytorch_tests", pattern="test_*.py")
-        # MCTQuantizersSuite.addTests(package_tests)
-        MCTQuantizersSuite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestPytorchGetInferableQuantizer))
+        # suiteList.append(unittest.TestLoader().loadTestsFromTestCase(TestPytorchGetInferableQuantizer))
+        package_tests = test_loader.discover("tests.pytorch_tests", pattern="test_*.py")
+        MCTQuantizersSuite.addTests(package_tests)
 
     unittest.TextTestRunner(verbosity=0).run(MCTQuantizersSuite)
