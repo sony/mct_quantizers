1862ac884bf7c596aed33984420297cdb4166978
reuvenp <reuvenp@altair-semi.com>
2023-08-07 16:49:00 +0300

    Add comments to get session options function

--- mct_quantizers/pytorch/onnxruntime_session_options.py
+++ mct_quantizers/pytorch/onnxruntime_session_options.py
@@ -2,7 +2,11 @@ import onnxruntime as ort
 from onnxruntime_extensions import get_library_path
 
 
-def get_ort_session_options():
+def get_ort_session_options() -> ort.SessionOptions:
+    """
+    :return: Session options for loading onnxruntime inference session with custom implementation
+    of onnx ops.
+    """
     opt = ort.SessionOptions()
     opt.register_custom_ops_library(get_library_path())
     return opt
