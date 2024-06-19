b98539db62867bbe567f83139f0f4ad5b9bdd74e
Reuven <44209964+reuvenperetz@users.noreply.github.com>
2024-06-19 15:09:29 +0300

    Update __init__.py
    Update version to 1.5.1

--- mct_quantizers/__init__.py
+++ mct_quantizers/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 
 
 from mct_quantizers.common.base_inferable_quantizer import QuantizationTarget, BaseInferableQuantizer, mark_quantizer
