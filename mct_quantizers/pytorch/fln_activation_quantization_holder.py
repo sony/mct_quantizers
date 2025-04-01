7f1c7c3f4df25d1b5b9ae1c9ea011a8ab847701b
kenta kawasaki <kawasaki.kenta@miraxia.com>
2025-04-01 13:29:57 +0900

    Adding FLNActivationQuantizationHolder and PreservingActivationQuantizationHolder (#114)
    * adding new Pytorch Activation Quantization Holders.
    add FLN feature to Activation Holder and include tests.
    Add Activation Holder for preserving quantizers and include tests.
    
    * Fixed module and test based on review feedback
    adding test patterns
    correction of argument description
    adding bypass flag
    
    * Remove constant value and Fixed message
    remove constant value
    correction of argument description for PytorchActivationQuantizationHolder class
    adding a test for the output of quantization holder before and after saving

---
+++ mct_quantizers/pytorch/fln_activation_quantization_holder.py
@@ -0,0 +1,63 @@
+# Copyright 2025 Sony Semiconductor Israel, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from mct_quantizers.common.base_inferable_quantizer import BaseInferableQuantizer
+from mct_quantizers.common.constants import FOUND_TORCH
+from mct_quantizers.pytorch.activation_quantization_holder import PytorchActivationQuantizationHolder
+from mct_quantizers.logger import Logger
+
+if FOUND_TORCH:
+    import torch
+
+    class PytorchFLNActivationQuantizationHolder(PytorchActivationQuantizationHolder):
+        """
+        Pytorch module to hold an activation quantizer and quantize during inference.
+        """
+        def __init__(self,
+                     activation_holder_quantizer: BaseInferableQuantizer,
+                     quantization_bypass: bool = False,
+                     **kwargs):
+            """
+
+            Args:
+                activation_holder_quantizer: Quantizer to use during inference.
+                quantization_bypass: Indicates whether to bypass quantization for the activation holder.
+                **kwargs: Key-word arguments used by torch.nn.Module.
+            """
+
+            super(PytorchFLNActivationQuantizationHolder, self).__init__(activation_holder_quantizer=activation_holder_quantizer,
+                                                                         **kwargs)
+            self.quantization_bypass = quantization_bypass
+
+        def forward(self, inputs):
+            """
+            Quantizes the input tensor using the activation quantizer of class PytorchFLNActivationQuantizationHolder.
+
+            Args:
+                inputs: Input tensors to quantize with the activation quantizer.
+
+            Returns: Output of the activation quantizer (quantized input tensor or input tensor).
+
+            """
+            if self.quantization_bypass:
+                return inputs
+            return super().forward(inputs)
+
+else:
+    class PytorchFLNActivationQuantizationHolder:  # pragma: no cover
+        def __init__(self, *args, **kwargs):
+            Logger.critical('Installing Pytorch is mandatory '
+                            'when using PytorchFLNActivationQuantizationHolder. '
+                            'Could not find the torch package.')  # pragma: no cover
